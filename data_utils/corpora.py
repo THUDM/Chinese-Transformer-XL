1c65f6919448136a7d6571e91d9fa61c5ca373af
duzx <duzx16@mails.tsinghua.edu.cn>
2021-04-03 10:25:27 +0800

    Add support for finetunee

--- data_utils/corpora.py
+++ data_utils/corpora.py
@@ -331,15 +331,20 @@ class wikipedia(PromptReader):
         prompt, text = cls.process_sample("", tokenizer, tokenize), cls.process_sample(text, tokenizer, tokenize)
         return [prompt], [text]
 
-class TestDataset(PromptReader):
-    PATH = '/root/data/test.json'
-    assert_str = "make sure to set PATH for wikipedia data_utils/corpora.py"
 
-    @classmethod
-    def process_line(cls, data, tokenizer, tokenize):
-        prompt, text = data['prompt'], data['text']
-        prompt, text = cls.process_sample(prompt, tokenizer, tokenize), cls.process_sample(text, tokenizer, tokenize)
-        return [prompt], [text]
+def get_finetune_dataset(path):
+    class FinetuneDataset(PromptDataset):
+        PATH = path
+        assert_str = f"File not exists at {PATH}"
+
+        @classmethod
+        def process_line(cls, data, tokenizer, tokenize):
+            prompt, text = data['prompt'], data['text']
+            prompt, text = cls.process_sample(prompt, tokenizer, tokenize), cls.process_sample(text, tokenizer,
+                                                                                               tokenize)
+            return [prompt], [text]
+
+    return FinetuneDataset
 
 
 NAMED_CORPORA = {
@@ -348,6 +353,5 @@ NAMED_CORPORA = {
     'webtext': webtext,
     "zhihu": zhihu,
     "zhidao": zhidao,
-    "baike": baike,
-    "test": TestDataset
+    "baike": baike
 }
